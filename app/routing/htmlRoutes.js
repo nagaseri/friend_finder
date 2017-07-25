46dadd095680a26b0228bfabfa44cc40d189a388
Eri <eriinwanderland@calvisitor-10-105-150-213.calvisitor.1918.berkeley.edu>
2017-07-25 09:11:52 -0700

    fixing functions

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,17 +2,16 @@
 // DEPENDENCIES
 //=================================================================================================
 var path = require("path");
-
 //=================================================================================================
 // ROUTING
 //=================================================================================================
 module.exports = function (app){
 	//go to survey page when url ends with /survey
 	app.get("/survey", function(req, res){
-		res.sendFile(path.join(__dirname, "public/../survey.html"));
+		res.sendFile(path.join(__dirname, "../survey.html"));
 	});
 	//go to home by default 
 	app.use("/", function(req, res){
-		res.sendFile(path.join(__dirname, "public/../home.html"));
+		res.sendFile(path.join(__dirname, "../home.html"));
 	});
 };
